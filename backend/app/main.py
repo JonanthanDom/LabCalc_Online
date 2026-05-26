28845d2cafc4e338ca13f8a9f59103c67143df0b
Jonathan Silva <jonathan.dom@gmail.com>
2026-05-26 14:21:08 -0300

    teste de rota proteinuria

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 
 from routes.hba1c import router as hba1c_router
-##from routes.proteinuria24h import router as proteinuria_router
+from routes.proteinuria24h import router as proteinuria_router
 ##from routes.relacao_prot_creatinina import router as rpc_router
 
 app = FastAPI()
@@ -23,5 +23,5 @@ def home():
 
 
 app.include_router(hba1c_router)
-##app.include_router(proteinuria_router)
+app.include_router(proteinuria_router)
 ##app.include_router(rpc_router)
