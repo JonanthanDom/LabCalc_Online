06eccaf04c331535cf463824ecd29075f20184d1
Jonathan Silva <jonathan.dom@gmail.com>
2026-05-26 14:18:15 -0300

    teste de rotas

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
 
-from routes.hba1c import router as hba1c_router
-from routes.proteinuria24h import router as proteinuria_router
-from routes.relacao_prot_creatinina import router as rpc_router
+##from routes.hba1c import router as hba1c_router
+##from routes.proteinuria24h import router as proteinuria_router
+##from routes.relacao_prot_creatinina import router as rpc_router
 
 app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
@@ -22,6 +22,6 @@ def home():
     return {"LabCalc": "online"}
 
 
-app.include_router(hba1c_router)
-app.include_router(proteinuria_router)
-app.include_router(rpc_router)
+##app.include_router(hba1c_router)
+##app.include_router(proteinuria_router)
+##app.include_router(rpc_router)
