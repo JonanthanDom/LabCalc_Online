6fada04fe2aa637b374ec2d0b18b974dbdd3378f
Jonathan Silva <jonathan.dom@gmail.com>
2026-05-26 14:19:43 -0300

    teste de rota hba1c

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 
-##from routes.hba1c import router as hba1c_router
+from routes.hba1c import router as hba1c_router
 ##from routes.proteinuria24h import router as proteinuria_router
 ##from routes.relacao_prot_creatinina import router as rpc_router
 
@@ -22,6 +22,6 @@ def home():
     return {"LabCalc": "online"}
 
 
-##app.include_router(hba1c_router)
+app.include_router(hba1c_router)
 ##app.include_router(proteinuria_router)
 ##app.include_router(rpc_router)
