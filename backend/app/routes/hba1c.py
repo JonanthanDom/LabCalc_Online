d21659f385e7476be88e8cd020a67f613df2b003
Jonathan Silva <jonathan.dom@gmail.com>
2026-05-26 13:58:35 -0300

    .

--- backend/app/routes/hba1c.py
+++ backend/app/routes/hba1c.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from typing import Optional
 
-from backend.app.services.bioquimica import calcular_hba1c_service
+from services.bioquimica import calcular_hba1c_service
 
 router = APIRouter()
 
